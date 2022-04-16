8d6e16b32593c8ac5a5003f5b0999665a84611c4
echoeyecodes <femiobajuluwa@gmail.com>
2022-04-16 21:40:18 +0100

    moved livedata initialization to constructor

--- app/src/main/java/com/echoeyecodes/dobby/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/echoeyecodes/dobby/viewmodel/MainActivityViewModel.kt
@@ -3,22 +3,25 @@ package com.echoeyecodes.dobby.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
 import com.echoeyecodes.dobby.db.models.FileDBModel
 import com.echoeyecodes.dobby.repository.FileRepository
+import com.echoeyecodes.dobby.utils.FileUtils
 import com.echoeyecodes.dobby.utils.NetworkState
 
 class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
     private val fileRepository = FileRepository(application)
+    private val files: LiveData<List<FileDBModel>> = fileRepository.getFilesLiveData()
 
     fun fetchData(url: String) {
         fileRepository.fetchFile(url)
     }
 
-    fun getNetworkState():LiveData<NetworkState>{
+    fun getNetworkState(): LiveData<NetworkState> {
         return fileRepository.getNetworkState()
     }
 
     fun getFilesLiveData(): LiveData<List<FileDBModel>> {
-        return fileRepository.getFilesLiveData()
+        return files
     }
 }
